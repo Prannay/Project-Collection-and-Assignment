9ed3c43cb5c4bb9cdcb861abcfc9ea9263d73524
Jasmeet Singh <jasmeet13n@users.noreply.github.com>
2015-04-17 00:21:49 -0500

    Add Database seeds for Users and Projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Administrator",
+             email: "admin@example.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+User.create!(name:  "Jasmeet Singh",
+             email: "jasmeet13n@tamu.edu",
+             password:              "iiit123",
+             password_confirmation: "iiit123",
+             admin: false)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+50.times do |n|
+  title  = Faker::Company.catch_phrase
+  organization = Faker::Company.name
+  contact = Faker::Name.name + "  " + Faker::PhoneNumber.cell_phone
+  description = Faker::Lorem.paragraph
+  oncampus = n%3 == 0 ? false : true
+  islegacy = n%4 == 0 ? false : true
+  Project.create!(title:  title,
+                  organization: organization,
+                  contact: contact,
+                  description: description,
+                  oncampus: oncampus,
+                  islegacy: islegacy)
+end
+
