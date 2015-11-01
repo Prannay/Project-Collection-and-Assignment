21924ac581724a224c804c31b68c69fbfb4ba0f4
Phakpoom Chinprutthiwong <cpx0rpc@gmail.com>
2015-11-01 02:24:56 -0600

    heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
-#require 'securerandom'
+require 'securerandom'
 class StaticPagesController < ApplicationController
   def home
   end
@@ -23,8 +23,7 @@ class StaticPagesController < ApplicationController
 			remember(user)
 			redirect_back_or user
 		else
-			#passwd = SecureRandom.hex
-			passwd = "SDKLFJ3423df"
+			passwd = SecureRandom.hex
 			user = User.new(:name => "Please Update", :email => email, :password_digest => passwd)	
 		  if user.save
 		     log_in user
